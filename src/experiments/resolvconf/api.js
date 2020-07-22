67cb20abaa61a504a397b891b7f74ce79338ce73
Austin Hounsel <austin.hounsel@gmail.com>
2020-07-22 11:42:57 -0400

    Nicely indent resolvconf/api.js

--- src/experiments/resolvconf/api.js
+++ src/experiments/resolvconf/api.js
@@ -11,44 +11,44 @@ var resolvconf = class resolvconf extends ExtensionAPI {
     return {
       experiments: {
         resolvconf: {
-        async readNameserversMac() {
+          async readNameserversMac() {
             let string1 = await OS.File.read("/etc/resolv.conf", { "encoding": "utf-8" });
             let lines = string1.split("\n");
 
             let nameservers = [];
             for (var i = 0; i < lines.length; i++) {
-                let line = lines[i];
-                if (line.startsWith("nameserver")) {
-                  let ns = line.split(" ")[1];
-                  nameservers.push(ns);
-                }
+              let line = lines[i];
+              if (line.startsWith("nameserver")) {
+                let ns = line.split(" ")[1];
+                nameservers.push(ns);
+              }
             }
             return nameservers;
-      },
+          },
 
-	  async readNameserversWin() {
-	       let nameservers = [];
-	       let rootKey = 0x80000002;
-	       let path = "SYSTEM\\CurrentControlSet\\Services\\Tcpip\\Parameters"
-               let key = Cc["@mozilla.org/windows-registry-key;1"].createInstance(
-                 Ci.nsIWindowsRegKey
-               );
+          async readNameserversWin() {
+            let nameservers = [];
+            let rootKey = 0x80000002;
+            let path = "SYSTEM\\CurrentControlSet\\Services\\Tcpip\\Parameters"
+            let key = Cc["@mozilla.org/windows-registry-key;1"].createInstance(
+              Ci.nsIWindowsRegKey
+            );
 
-               // Reading the registry may throw an exception, and that's ok.  In error
-               // cases, we just leave ourselves in the empty state.
-               try {
-                 key.open(rootKey, path, Ci.nsIWindowsRegKey.ACCESS_READ);
-		 let nameservers_registry = key.readStringValue("DhcpNameServer");
-		 nameservers = nameservers_registry.split(" ");
-               } catch (e) {
-		 console.log(e);
-               } finally {
-                 key.close();
-	       }
-               return nameservers;
-	 }
-       }
-     }
-   };
- }
+            // Reading the registry may throw an exception, and that's ok.  In error
+            // cases, we just leave ourselves in the empty state.
+            try {
+              key.open(rootKey, path, Ci.nsIWindowsRegKey.ACCESS_READ);
+              let nameservers_registry = key.readStringValue("DhcpNameServer");
+              nameservers = nameservers_registry.split(" ");
+            } catch (e) {
+              console.log(e);
+            } finally {
+              key.close();
+            }
+            return nameservers;
+          }
+        }
+      }
+    };
+  }
 };
