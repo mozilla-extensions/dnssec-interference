bb6d627d50ddf225dfc545270bd02585d3a9de8f
Austin Hounsel <austin.hounsel@gmail.com>
2020-07-27 15:37:38 -0400

    Add basic re-transmission behavior

--- src/dns-test.js
+++ src/dns-test.js
@@ -2,10 +2,16 @@
 const dnsPacket = require("dns-packet-fork");
 
 const rrtypes = ['A', 'AAAA', 'RRSIG', 'DNSKEY', 'SMIMEA', 'HTTPS', 'NEW'];
+const resolvconf_timeout = 5000; // 5 seconds
+const resolvconf_attempts = 2;
+
+var nameservers = [];
+var dns_responses = {};
 var query_proto;
 
 const rollout = {
-    async sendQuery(domain, nameserver, rrtype, useIPv4) {
+    async sendQuery(domain, nameservers, rrtype, useIPv4) {
+        let written = 0;
         const buf = dnsPacket.encode({
             type: 'query',
             id: 1,
@@ -24,7 +30,22 @@ const rollout = {
         console.log('Query decoded');
         console.log(dnsPacket.decode(buf));
 
-        return await browser.experiments.udpsocket.sendDNSQuery(nameserver, buf, rrtype, useIPv4);
+        // Keep re-transmitting according to default resolv.conf behavior,
+        // checking if we have a DNS response for the RR type yet
+        for (let i = 0; i < nameservers.length; i++) {
+            for (let j = 0; j < resolvconf_attempts; j++) {
+                let nameserver = nameservers[i];
+                let written = await browser.experiments.udpsocket.sendDNSQuery(nameserver, buf, rrtype, useIPv4);
+                await sleep(resolvconf_timeout);
+
+                if (!isUndefined(dns_responses[rrtype])) {
+                    break;
+                } else {
+                    console.log("Need to re-transmit");
+                }
+            }
+            return written;
+        }
     },
 
     processDNSResponse(responseBytes, rrtype, usedIPv4) {
@@ -33,6 +54,7 @@ const rollout = {
          * TODO: Determine if the bucket should be "event" or "main", rather 
          * than "dnssec-experiment"
          */
+        dns_responses[rrtype] = responseBytes;
         // sendResponsePing([123], rrtype, usedIPv4);
         console.log(responseBytes, rrtype, (usedIPv4 = true ? "IPv4" : "IPv4"));
 
@@ -40,7 +62,6 @@ const rollout = {
         decodedResponse = dnsPacket.decode(responseBytes);
         console.log('Response decoded');
         console.log(decodedResponse);
-
     }
 }
 
@@ -100,26 +121,26 @@ function sendResponsePing(_responseBytes, _rrtype, _usedIPv4) {
 }
 
 async function init() {
-    let nameservers = await browser.experiments.resolvconf.readNameserversMac();
-    // let nameservers = await browser.experiments.resolvconf.readNameserversWin();
+    nameservers = await browser.experiments.resolvconf.readNameserversMac();
+    // nameservers = await browser.experiments.resolvconf.readNameserversWin();
 
     if (!Array.isArray(nameservers) || nameservers.length == 0) {
         sendNameserversErrorPing();
         return;
     }
   
-    let ns_ipv4;
-    let ns_ipv6;
+    let nameservers_ipv4 = [];
+    let nameservers_ipv6 = [];
     for (var i = 0; i < nameservers.length; i++) {
         let ns = nameservers[i];
-        if (isUndefined(ns_ipv4) && !isUndefined(ns) && ns.includes(".")) {
-            ns_ipv4 = ns;
-        } else if (isUndefined(ns_ipv6) && !isUndefined(ns) && ns.includes(":")) {
-            ns_ipv6 = ns;
+        if (!isUndefined(ns) && ns.includes(".")) {
+            nameservers_ipv4.push(ns);
+        } else if (!isUndefined(ns) && ns.includes(":")) {
+            nameservers_ipv6.push(ns);
         }
     }
-    console.log("IPv4 resolver: ", ns_ipv4);
-    console.log("IPv6 resolver: ", ns_ipv6);
+    console.log("IPv4 resolvers: ", nameservers_ipv4);
+    console.log("IPv6 resolvers: ", nameservers_ipv6);
 
     // TODO: If we use IPv4 and IPv6 sockets in the future, then we may 
     // want to split openSocket() into two different methods. This would enable 
@@ -132,14 +153,14 @@ async function init() {
     }
     browser.experiments.udpsocket.onDNSResponseReceived.addListener(rollout.processDNSResponse);
 
-    if (!isUndefined(ns_ipv4)) {
+    if (nameservers_ipv4.length > 0) {
         let written;
         for (let i = 0; i < rrtypes.length; i++) {
           let rrtype = rrtypes[i];
           if (rrtype == 'HTTPS') {
-            written = await rollout.sendQuery('cloudflare-http1.com', ns_ipv4, rrtype, true);
+            written = await rollout.sendQuery('cloudflare-http1.com', nameservers_ipv4, rrtype, true);
           } else {
-            written = await rollout.sendQuery('example.com', ns_ipv4, rrtype, true);
+            written = await rollout.sendQuery('example.com', nameservers_ipv4, rrtype, true);
           }
 
           if (written <= 0) {
@@ -147,10 +168,15 @@ async function init() {
           }
         }
     }
+    console.log(dns_responses);
 }
 
 function isUndefined(x) {
    return (typeof(x) === 'undefined' || x === null);
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 init();
