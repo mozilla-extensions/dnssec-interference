dc6d49753732a90c62f25226ecfdfccde6b3744a
Austin Hounsel <austin.hounsel@gmail.com>
2020-08-06 11:57:51 -0400

    Indent files

--- src/experiments/resolvconf/api.js
+++ src/experiments/resolvconf/api.js
@@ -6,41 +6,41 @@ const { OS } = ChromeUtils.import("resource://gre/modules/osfile.jsm");
 const dns_win_registry = "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\Tcpip\\Parameters\\DhcpNameServer"
 
 var resolvconf = class resolvconf extends ExtensionAPI {
-  getAPI(context) {
-    const {extension} = context;
-    return {
-      experiments: {
-        resolvconf: {
-          async readNameserversMac() {
-            let nameservers = [];
-            let resolvconf_string = await OS.File.read("/etc/resolv.conf", { "encoding": "utf-8" });
-            let lines = resolvconf_string.split("\n");
-            for (var i = 0; i < lines.length; i++) {
-              let line = lines[i];
-              if (line.startsWith("nameserver")) {
-                let ns = line.split(" ")[1];
-                nameservers.push(ns);
-              }
+    getAPI(context) {
+        const {extension} = context;
+        return {
+            experiments: {
+                resolvconf: {
+                    async readNameserversMac() {
+                        let nameservers = [];
+                        let resolvconf_string = await OS.File.read("/etc/resolv.conf", { "encoding": "utf-8" });
+                        let lines = resolvconf_string.split("\n");
+                        for (var i = 0; i < lines.length; i++) {
+                            let line = lines[i];
+                            if (line.startsWith("nameserver")) {
+                                let ns = line.split(" ")[1];
+                                nameservers.push(ns);
+                            }
+                        }
+                        return nameservers;
+                    },
+
+                    async readNameserversWin() {
+                        let nameservers = [];
+                        let rootKey = 0x80000002;
+                        let path = "SYSTEM\\CurrentControlSet\\Services\\Tcpip\\Parameters"
+                        let key = Cc["@mozilla.org/windows-registry-key;1"].createInstance(
+                            Ci.nsIWindowsRegKey
+                        );
+
+                        key.open(rootKey, path, Ci.nsIWindowsRegKey.ACCESS_READ);
+                        let nameservers_registry = key.readStringValue("DhcpNameServer");
+                        nameservers = nameservers_registry.split(" ");
+                        key.close();
+                        return nameservers;
+                    }
+                }
             }
-            return nameservers;
-          },
-
-          async readNameserversWin() {
-            let nameservers = [];
-            let rootKey = 0x80000002;
-            let path = "SYSTEM\\CurrentControlSet\\Services\\Tcpip\\Parameters"
-            let key = Cc["@mozilla.org/windows-registry-key;1"].createInstance(
-              Ci.nsIWindowsRegKey
-            );
-
-            key.open(rootKey, path, Ci.nsIWindowsRegKey.ACCESS_READ);
-            let nameservers_registry = key.readStringValue("DhcpNameServer");
-            nameservers = nameservers_registry.split(" ");
-            key.close();
-            return nameservers;
-          }
-        }
-      }
-    };
-  }
+        };
+    }
 };
--- src/experiments/tcpsocket/api.js
+++ src/experiments/tcpsocket/api.js
@@ -18,143 +18,143 @@ const { TCPSocket } = Cu.getGlobalForObject(
  * invoked.
  */
 function listenForEventsOnSocket(socket, socketType) {
-  let wantDataLength = null;
-  let wantDataAndClose = false;
-  let pendingResolve = null;
-  let receivedEvents = [];
-  let receivedData = null;
-  let handleGenericEvent = function(event) {
-    console.log(event);
-    console.log("(" + socketType + " event: " + event.type + ")\n");
-    if (pendingResolve && wantDataLength === null) {
-      pendingResolve(event);
-      pendingResolve = null;
-    } else {
-      receivedEvents.push(event);
-    }
-  };
+    let wantDataLength = null;
+    let wantDataAndClose = false;
+    let pendingResolve = null;
+    let receivedEvents = [];
+    let receivedData = null;
+    let handleGenericEvent = function(event) {
+        console.log(event);
+        console.log("(" + socketType + " event: " + event.type + ")\n");
+        if (pendingResolve && wantDataLength === null) {
+            pendingResolve(event);
+            pendingResolve = null;
+        } else {
+            receivedEvents.push(event);
+        }
+    };
 
-  socket.onopen = handleGenericEvent;
-  socket.ondrain = handleGenericEvent;
-  socket.onerror = handleGenericEvent;
-  socket.onclose = function(event) {
-    if (!wantDataAndClose) {
-      handleGenericEvent(event);
-    } else if (pendingResolve) {
-      console.log("(" + socketType + " event: close)\n");
-      pendingResolve(receivedData);
-      pendingResolve = null;
-      wantDataAndClose = false;
-    }
-  };
-  socket.ondata = function(event) {
-    console.log(
-      "(" +
-        socketType +
-        " event: " +
-        event.type +
-        " length: " +
-        event.data.byteLength +
-        ")\n"
-    );
-    
-    var arr = new Uint8Array(event.data);
-    if (receivedData === null) {
-      receivedData = arr;
-    } else {
-      console.log(receivedData);
-      receivedData = concatUint8Arrays(receivedData, arr);
-    }
-    if (wantDataLength !== null && receivedData.length >= wantDataLength) {
-      pendingResolve(receivedData);
-      pendingResolve = null;
-      receivedData = null;
-      wantDataLength = null;
-    }
-  };
+    socket.onopen = handleGenericEvent;
+    socket.ondrain = handleGenericEvent;
+    socket.onerror = handleGenericEvent;
+    socket.onclose = function(event) {
+        if (!wantDataAndClose) {
+            handleGenericEvent(event);
+        } else if (pendingResolve) {
+            console.log("(" + socketType + " event: close)\n");
+            pendingResolve(receivedData);
+            pendingResolve = null;
+            wantDataAndClose = false;
+        }
+    };
+    socket.ondata = function(event) {
+        console.log(
+            "(" +
+            socketType +
+            " event: " +
+            event.type +
+            " length: " +
+            event.data.byteLength +
+            ")\n"
+        );
 
-  return {
-    /**
-     * Return a Promise that will be resolved with the next (non-data) event
-     * received by the socket.  If there are queued events, the Promise will
-     * be immediately resolved (but you won't see that until a future turn of
-     * the event loop).
-     */
-    waitForEvent() {
-      if (pendingResolve) {
-        throw new Error("only one wait allowed at a time.");
-      }
+        var arr = new Uint8Array(event.data);
+        if (receivedData === null) {
+            receivedData = arr;
+        } else {
+            console.log(receivedData);
+            receivedData = concatUint8Arrays(receivedData, arr);
+        }
+        if (wantDataLength !== null && receivedData.length >= wantDataLength) {
+            pendingResolve(receivedData);
+            pendingResolve = null;
+            receivedData = null;
+            wantDataLength = null;
+        }
+    };
 
-      if (receivedEvents.length) {
-        return Promise.resolve(receivedEvents.shift());
-      }
+    return {
+        /**
+         * Return a Promise that will be resolved with the next (non-data) event
+         * received by the socket.  If there are queued events, the Promise will
+         * be immediately resolved (but you won't see that until a future turn of
+         * the event loop).
+         */
+        waitForEvent() {
+            if (pendingResolve) {
+                throw new Error("only one wait allowed at a time.");
+            }
 
-      console.log("(" + socketType + " waiting for event)\n");
-      return new Promise(function(resolve, reject) {
-        pendingResolve = resolve;
-      });
-    },
-    /**
-     * Return a Promise that will be resolved with a Uint8Array of at least the
-     * given length.  We buffer / accumulate received data until we have enough
-     * data.  Data is buffered even before you call this method, so be sure to
-     * explicitly wait for any and all data sent by the other side.
-     */
-    waitForDataWithAtLeastLength(length) {
-      if (pendingResolve) {
-        throw new Error("only one wait allowed at a time.");
-      }
-      if (receivedData && receivedData.length >= length) {
-        let promise = Promise.resolve(receivedData);
-        receivedData = null;
-        return promise;
-      }
-      console.log("(" + socketType + " waiting for " + length + " bytes)\n");
-      return new Promise(function(resolve, reject) {
-        pendingResolve = resolve;
-        wantDataLength = length;
-      });
-    },
-    waitForAnyDataAndClose() {
-      if (pendingResolve) {
-        throw new Error("only one wait allowed at a time.");
-      }
+            if (receivedEvents.length) {
+                return Promise.resolve(receivedEvents.shift());
+            }
 
-      return new Promise(function(resolve, reject) {
-        pendingResolve = resolve;
-        // we may receive no data before getting close, in which case we want to
-        // return an empty array
-        receivedData = new Uint8Array();
-        wantDataAndClose = true;
-      });
-    },
-  };
+            console.log("(" + socketType + " waiting for event)\n");
+            return new Promise(function(resolve, reject) {
+                pendingResolve = resolve;
+            });
+        },
+        /**
+         * Return a Promise that will be resolved with a Uint8Array of at least the
+         * given length.  We buffer / accumulate received data until we have enough
+         * data.  Data is buffered even before you call this method, so be sure to
+         * explicitly wait for any and all data sent by the other side.
+         */
+        waitForDataWithAtLeastLength(length) {
+            if (pendingResolve) {
+                throw new Error("only one wait allowed at a time.");
+            }
+            if (receivedData && receivedData.length >= length) {
+                let promise = Promise.resolve(receivedData);
+                receivedData = null;
+                return promise;
+            }
+            console.log("(" + socketType + " waiting for " + length + " bytes)\n");
+            return new Promise(function(resolve, reject) {
+                pendingResolve = resolve;
+                wantDataLength = length;
+            });
+        },
+        waitForAnyDataAndClose() {
+            if (pendingResolve) {
+                throw new Error("only one wait allowed at a time.");
+            }
+
+            return new Promise(function(resolve, reject) {
+                pendingResolve = resolve;
+                // we may receive no data before getting close, in which case we want to
+                // return an empty array
+                receivedData = new Uint8Array();
+                wantDataAndClose = true;
+            });
+        },
+    };
 }
 
 var tcpsocket = class tcpsocket extends ExtensionAPI {
-  getAPI(context) {
-    const {extension} = context;
-    return {
-      experiments: {
-        tcpsocket: {
-          async sendDNSQuery(addr, buf) {
-            let tcp_socket = new TCPSocket(addr, 53, { binaryType: "arraybuffer" });
-            let tcp_event_queue = listenForEventsOnSocket(tcp_socket, "client");
-           
-            // Wait until the socket has opened a connection to the DNS server
-            let nextEvent = (await tcp_event_queue.waitForEvent()).type;
-            if (nextEvent == "open" && tcp_socket.readyState == "open") {
-                console.log("client opened socket and readyState is open");
-            } else {
-                throw new Error("Could not open TCP socket");
-            }
+    getAPI(context) {
+        const {extension} = context;
+        return {
+            experiments: {
+                tcpsocket: {
+                    async sendDNSQuery(addr, buf) {
+                        let tcp_socket = new TCPSocket(addr, 53, { binaryType: "arraybuffer" });
+                        let tcp_event_queue = listenForEventsOnSocket(tcp_socket, "client");
 
-            tcp_socket.send(buf.buffer, buf.byteOffset, buf.byteLength);
-            let answer = await tcp_event_queue.waitForDataWithAtLeastLength(buf.byteLength);
-            return answer;
-          }
-        },
-      },
-    };
-  }
+                        // Wait until the socket has opened a connection to the DNS server
+                        let nextEvent = (await tcp_event_queue.waitForEvent()).type;
+                        if (nextEvent == "open" && tcp_socket.readyState == "open") {
+                            console.log("client opened socket and readyState is open");
+                        } else {
+                            throw new Error("Could not open TCP socket");
+                        }
+
+                        tcp_socket.send(buf.buffer, buf.byteOffset, buf.byteLength);
+                        let answer = await tcp_event_queue.waitForDataWithAtLeastLength(buf.byteLength);
+                        return answer;
+                    }
+                },
+            },
+        };
+    }
 };
