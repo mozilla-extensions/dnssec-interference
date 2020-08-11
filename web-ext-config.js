2cdeae9a341457718590b1274590ead49320e9a4
Austin Hounsel <austin.hounsel@gmail.com>
2020-08-11 16:24:32 -0400

    Improve package.json

---
+++ web-ext-config.js
@@ -0,0 +1,11 @@
+/* eslint-env node */
+
+const defaultConfig = {
+  sourceDir: "./src/",
+  ignoreFiles: [".DS_Store"],
+  build: {
+    overwriteDest: true,
+  }
+};
+
+module.exports = defaultConfig;
