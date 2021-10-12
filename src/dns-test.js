b08b1602f168445939aa95ac9e4a231739a5b2cc
Austin Hounsel <austin.hounsel@gmail.com>
2021-10-12 18:50:24 -0400

    Set CD bit for A w/ DO=1 queries

--- src/dns-test.js
+++ src/dns-test.js
@@ -91,7 +91,7 @@ function encodeUDPQuery(domain, rrtype, dnssec_ok) {
             type: 'query',
             // Generate a random transaction ID between 0 and 65535
             id: Math.floor(Math.random() * (MAX_TXID - MIN_TXID + 1)) + MIN_TXID,
-            flags: DNS_PACKET.RECURSION_DESIRED,
+            flags: DNS_PACKET.RECURSION_DESIRED | DNS_PACKET.CHECKING_DISABLED,
             questions: [{ type: rrtype, name: domain }],
             additionals: [{ type: 'OPT', name: '.', udpPayloadSize: UDP_PAYLOAD_SIZE, flags: DNS_PACKET.DNSSEC_OK }]
         });
@@ -118,7 +118,7 @@ function encodeTCPQuery(domain, rrtype, dnssec_ok) {
             type: 'query',
             // Generate a random transaction ID between 0 and 65535
             id: Math.floor(Math.random() * (MAX_TXID - MIN_TXID + 1)) + MIN_TXID,
-            flags: DNS_PACKET.RECURSION_DESIRED,
+            flags: DNS_PACKET.RECURSION_DESIRED | DNS_PACKET.CHECKING_DISABLED,
             questions: [{ type: rrtype, name: domain }],
             additionals: [{ type: 'OPT', name: '.', flags: DNS_PACKET.DNSSEC_OK }]
         });
